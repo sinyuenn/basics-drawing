b9e402fdc13cf9a1f8149961bb4e5385948fbe77
sinyuenn <sinyuen24@gmail.com>
2021-10-31 17:03:25 +0800

    added thumbs up code

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "";
+  var counter = 0;
+
+  while (counter < input) {
+    var columnCounter = 0;
+    while (columnCounter < input) {
+      myOutputValue = myOutputValue + "👍";
+      columnCounter = columnCounter + 1;
+    }
+    myOutputValue = myOutputValue + "<br>";
+    counter = counter + 1;
+  }
+
   return myOutputValue;
 };
